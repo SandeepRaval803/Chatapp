e1e824e8ec5eaec164c9166966faabb73520a507
Sandeep Raval <85988613+SandeepRaval803@users.noreply.github.com>
2024-09-02 21:01:16 +0530

    init

---
+++ App.tsx
@@ -0,0 +1,98 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  ActivityIndicator
+} from 'react-native';
+
+import * as Google from "expo-auth-session/providers/google";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithCredential,
+} from "firebase/auth";
+import "react-native-gesture-handler";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { auth } from './firebaseConfig';
+import Navigation from './Navigation';
+import SignInScreen from './src/screens/SignInScreen';
+
+function App(): React.JSX.Element {
+  const [userInfo, setUserInfo] = React.useState();
+  const [loading, setLoading] = React.useState(false);
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    iosClientId: "",
+    androidClientId: "",
+  });
+
+  const getLocalUser = async () => {
+    try {
+      setLoading(true);
+      const userJSON = await AsyncStorage.getItem("@user");
+      const userData = userJSON ? JSON.parse(userJSON) : null;
+      setUserInfo(userData);
+    } catch (e) {
+      console.log(e, "Error getting local user");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  React.useEffect(() => {
+    if (response?.type === "success") {
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential);
+    }
+  }, [response]);
+
+  React.useEffect(() => {
+    getLocalUser();
+    const unsub = onAuthStateChanged(auth, async (user:any) => {
+      if (user) {
+        await AsyncStorage.setItem("@user", JSON.stringify(user));
+        console.log(JSON.stringify(user, null, 2));
+        setUserInfo(user);
+      } else {
+        console.log("user not authenticated");
+      }
+    });
+    return () => unsub();
+  }, []);
+
+  if (loading)
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ActivityIndicator size={"large"} />
+      </View>
+    );
+  return userInfo ? <Navigation /> : <SignInScreen promptAsync={promptAsync} />;
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
